ac06e9b2e4833f94539c3fc6eda67c17dd029b0d
fu.wei <wei.fu@mtime.com>
2018-06-05 14:17:22 +0800

    引入 velocity

--- src/main/java/com/polar/admin/portal/controller/IndexController.java
+++ src/main/java/com/polar/admin/portal/controller/IndexController.java
@@ -1,23 +1,20 @@
 package com.polar.admin.portal.controller;
 
 import com.polar.admin.portal.dao.UserDao;
-import com.polar.admin.portal.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * Created by fuyuanpu on 2017/1/15.
  */
-@RestController
+@Controller
 public class IndexController {
     @Autowired
     UserDao userDao;
 
     @RequestMapping("index")
-    public List<User> index() {
-        return userDao.findAll();
+    public String index() {
+        return "index";
     }
 }
