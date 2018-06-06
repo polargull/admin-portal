23ec03831254bc7044a62978fa2d36343099fb46
fu.wei <wei.fu@mtime.com>
2018-06-06 18:33:59 +0800

    引入 thymeleaf

--- src/main/java/com/polar/admin/portal/AdminPortalApplication.java
+++ src/main/java/com/polar/admin/portal/AdminPortalApplication.java
@@ -2,8 +2,9 @@ package com.polar.admin.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class AdminPortalApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/polar/admin/portal/controller/IndexController.java
+++ src/main/java/com/polar/admin/portal/controller/IndexController.java
@@ -1,20 +1,19 @@
 package com.polar.admin.portal.controller;
 
-import com.polar.admin.portal.dao.UserDao;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by fuyuanpu on 2017/1/15.
  */
 @Controller
 public class IndexController {
-    @Autowired
-    UserDao userDao;
+//    @Autowired
+//    UserDao userDao;
 
     @RequestMapping("index")
-    public String index() {
-        return "index";
+    public ModelAndView index() {
+        return new ModelAndView("content");
     }
 }
